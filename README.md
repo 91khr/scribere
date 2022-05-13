fdeb5a0332e3b10d8da5557faa25b2bfea414ab1
Virginia Senioria <91khr@users.noreply.github.com>
2022-05-13 17:02:44 +0800

    init project with a skeleton

---
+++ README.md
@@ -0,0 +1,5 @@
+# scribere
+
+A literature programming library aiming at universality, extensibility, and document friendliness.
+
+[//modeline]: random:// " vim:set spell nofoldenable: "
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![doc = include_str!("../README.md")]
+#![warn(clippy::missing_docs_in_private_items)]
+#![warn(missing_docs)] // Duplicate, but this can report the missing earlier
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(rustdoc::all)]
+
+
+
+#[cfg(test)]
+mod tests {}
