f2dd8c23ab388dcef2d1e4b5a89bd0734a7352b5
Virginia Senioria <91khr@users.noreply.github.com>
2022-05-23 18:11:28 +0800

    feat write_blocks: error process and simple tests

--- src/write_blocks.rs
+++ src/write_blocks.rs
@@ -6,6 +6,8 @@ Write all code blocks in the iterator to the directory with the dispatcher.
 
 use std::io::Write;
 
+use thiserror::Error;
+
 use crate::codeblock::CodeBlock;
 use crate::directory::Directory;
 use crate::dispatch::Dispatch;
@@ -13,25 +15,43 @@ use crate::dispatch::Dispatch;
 
 
 /**
+The result of writing operations.
+*/
+#[derive(Debug, Error)]
+pub enum WriteError<E: std::error::Error> {
+    /// The path returned by dispatcher for the first code block is `None`.
+    #[error("dispatcher returned `None` for the first code block")]
+    NullPath,
+    /// The error returned from the directory.
+    #[error("directory error: {0}")]
+    DirError(E),
+    /// The error during writing.
+    #[error("IO error: {0}")]
+    IOError(std::io::Error),
+}
+
+
+
+/**
 Write all code blocks in the iterator to the directory with the dispatcher.
 */
-pub fn write_blocks<'a>(
+pub fn write_blocks<'a, Dir: Directory>(
     mut it: impl Iterator<Item = CodeBlock<'a>>,
-    dir: &mut impl Directory,
+    dir: &mut Dir,
     mut disp: impl Dispatch<'a>,
-) {
+) -> Result<(), WriteError<Dir::DirError>> {
     let mut blk = match it.next() {
         Some(b) => b,
-        None => return,
+        None => return Ok(()),
     };
-    let mut path = disp.dispatch(&blk).unwrap();
+    let mut path = disp.dispatch(&blk).ok_or(WriteError::NullPath)?;
     loop {
-        let mut writer = dir.open_append(path).unwrap();
+        let mut writer = dir.open_append(path).map_err(WriteError::DirError)?;
         loop {
-            writer.write_all(blk.content.as_bytes()).unwrap();
+            writer.write_all(blk.content.as_bytes()).map_err(WriteError::IOError)?;
             blk = match it.next() {
                 Some(b) => b,
-                None => return,
+                None => return Ok(()),
             };
             if let Some(p) = disp.dispatch(&blk) {
                 path = p;
@@ -45,6 +65,62 @@ pub fn write_blocks<'a>(
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
+    use crate::codeblock::CodeBlock;
+    use crate::directory::dummydir::DummyDir;
+    use crate::dispatch::ByAttr;
+    use crate::write_blocks;
+
     #[test]
-    fn some() {}
+    fn some() {
+        let mut dir = DummyDir::new();
+        write_blocks(
+            [
+                CodeBlock {
+                    lang: "",
+                    content: "block 1\n",
+                    attrs: vec![("a", "hello")],
+                },
+                CodeBlock {
+                    lang: "",
+                    content: "block 2\n",
+                    attrs: vec![("b", "hello")],
+                },
+                CodeBlock {
+                    lang: "",
+                    content: "block 3\n",
+                    attrs: vec![("a", "hi")],
+                },
+                CodeBlock {
+                    lang: "",
+                    content: "block 4\n",
+                    attrs: vec![("a", "hello")],
+                },
+                CodeBlock {
+                    lang: "",
+                    content: "block 5\n",
+                    attrs: vec![("b", "hello")],
+                },
+            ]
+            .into_iter(),
+            &mut dir,
+            ByAttr::new("a"),
+        )
+        .unwrap();
+        let mut dir = dir.dump().into_iter().collect::<Vec<_>>();
+        dir.sort();
+        assert_eq!(
+            dir,
+            [
+                (PathBuf::from("hello"), b"block 1\nblock 2\nblock 4\nblock 5\n".to_vec()),
+                (PathBuf::from("hi"), b"block 3\n".to_vec())
+            ]
+        );
+    }
+
+    #[test]
+    fn empty_blocks() {
+        write_blocks([].into_iter(), &mut DummyDir::new(), ByAttr::new("a")).unwrap();
+    }
 }
